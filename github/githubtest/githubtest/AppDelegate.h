46548711510f5b230a95087326e089889274c7aa
kanglekal <kanglekal@163.com>
2014-10-05 16:28:55 +0800

    Initial Commit

---
+++ github/githubtest/githubtest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  githubtest
+//
+//  Created by kal on 14-10-5.
+//  Copyright (c) 2014年 kal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ github/githubtest/githubtest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  githubtest
+//
+//  Created by kal on 14-10-5.
+//  Copyright (c) 2014年 kal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
